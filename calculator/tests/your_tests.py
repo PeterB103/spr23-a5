ce65aababb847b2bfd6aa281214ec93156dedacc
PeterB103 <pbenitez0402@gmail.com>
2023-05-17 10:56:16 -0700

    Created the MakeFile & 4 Tests

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -3,8 +3,10 @@ from calculator_adapter import run
 
 
 ### ADD AT LEAST TWO TESTS HERE!
+assert run("5 * 5").output == 25
+assert run("5 / 5").output == 1
+assert run("5 - 5").output == 0
+assert run("5 + 5").output == 10
 
-
-###
 
 print("All tests passed!")
